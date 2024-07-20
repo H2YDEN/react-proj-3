0fd47a47ed7f1bdf8d66ae729da5e5c34e747669
H2YDEN <h2yden2@gmail.com>
2024-07-20 02:54:09 -0700

    Completed

--- starter/src/components/Footer.jsx
+++ starter/src/components/Footer.jsx
@@ -1,23 +1,40 @@
 import React from "react";
+import styles from "./Header.module.css";
 import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
 
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className={styles.social_container}>
         <h5>Hit us up on our socials!</h5>
-        <div>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImFacebook size="1.5em" color="#3f3f3f" />
+        <div className={styles.social_icons}>
+          <a
+            href="https://www.devmountain.com"
+            target="_blank"
+            className={styles.circle}
+          >
+            <ImFacebook size="1.5em" color="#424242" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImInstagram size="1.5em" color="#3f3f3f" />
+          <a
+            href="https://www.devmountain.com"
+            target="_blank"
+            className={styles.circle}
+          >
+            <ImInstagram size="1.5em" color="#424242" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImTwitter size="1.5em" color="#3f3f3f" />
+          <a
+            href="https://www.devmountain.com"
+            target="_blank"
+            className={styles.circle}
+          >
+            <ImTwitter size="1.5em" color="#424242" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImGithub size="1.5em" color="#3f3f3f" />
+          <a
+            href="https://www.devmountain.com"
+            target="_blank"
+            className={styles.circle}
+          >
+            <ImGithub size="1.5em" color="#424242" />
           </a>
         </div>
       </div>
